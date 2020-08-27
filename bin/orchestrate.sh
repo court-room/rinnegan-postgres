5c7a764a5926f9a03f9d393fdd0c13d234101b38
onlinejudge95 <onlinejudge95@gmail.com>
2020-08-27 18:58:00 +0530

    Uses same steps as in other services

--- bin/orchestrate.sh
+++ bin/orchestrate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
-docker-compose up --build --detach
-docker-compose logs --follow postgres
+clear
+docker-compose build --compress --force-rm --parallel
+docker-compose up --detach --remove-orphans
