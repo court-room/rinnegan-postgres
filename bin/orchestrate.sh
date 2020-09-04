ac89bccb5081896ca4fa95c0b0fbbfb4b3155ec8
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-05 04:37:07 +0530

    Adds push step to local registry

--- bin/orchestrate.sh
+++ bin/orchestrate.sh
@@ -3,4 +3,7 @@
 clear
 docker-compose build --compress --force-rm --parallel
 docker-compose up --detach --remove-orphans
+
+docker push localhost:5000/postgres
+
 docker-compose logs --follow postgres
