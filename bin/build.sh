9bc7347f2d918941254157dbf431322bc50a3280
onlinejudge95 <44158581+onlinejudge95@users.noreply.github.com>
2020-09-05 15:12:08 +0530

    Integrates with the local registry (#10)
    * Adds push step to local registry
    
    * Adds to the registry network
    
    * Pushes the image to local registry
    
    * Adds push to local registry
    
    * Removes unnecessary flags
    
    * Removes cli param from build script

---
+++ bin/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+clear
+docker-compose build --compress --force-rm
+
+docker image tag rinnegan-postgres:latest localhost:6000/rinnegan-postgres:latest
+
+docker push localhost:6000/rinnegan-postgres:latest
