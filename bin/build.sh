49910da139103d288f8bd4e73a455ce4bf652352
onlinejudge95 <onlinejudge95@gmail.com>
2020-09-05 13:39:29 +0530

    Adds push to local registry

---
+++ bin/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+ACTION=$1
+
+clear
+docker-compose build --compress --force-rm --parallel
+
+docker image tag rinnegan-postgres:latest localhost:6000/rinnegan-postgres:latest
+
+docker push localhost:6000/rinnegan-postgres:latest
